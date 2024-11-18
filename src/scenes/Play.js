fa683ccb7ad4f915ef11cf5443e20b6513c785ca
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-11-17 18:19:11 -0800

    Button party!

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,33 +5,35 @@ class Play extends Phaser.Scene {
 
     init() {
         this.BUTTON_LAYER = 100
+        this.TILE_SIZE = 8
     }
 
     create() {
         // running checks
         console.log('%cPLAY SCENE :^)', testColor)
 
-        this.tickButton = this.constructButton(8, 8, 10, 6)
-        this.add.rectangle(0, 0, 8, 8, 0x00ff00).setOrigin(0)
-        this.add.rectangle(0, 8, 8, 8, 0x0000ff).setOrigin(0)
-        this.add.rectangle(0, 16, 8, 8, 0x00ff00).setOrigin(0)
+        this.tickButton = this.constructButton(this.TILE_SIZE, this.TILE_SIZE, 10, 6, 'press to tick time', this.moveTime)
     }
 
     update() {
-        if (Phaser.Input.Keyboard.JustDown(cursors.down)) {
-            console.log('This is how keys work!')
-        }
     }
 
-    constructButton(x, y, textSize, padding, text = 'default text') {
-        const content = this.add.text(x + padding/2, y + padding/2, text, { fontSize: `${textSize}px`, lineSpacing: 0 })
+    constructButton(x, y, textSize, padding, text = 'default text', result) {
+        const content = this.add.text(x + padding/2, y + padding/2, text, { fontSize: `${textSize - 2}px`, lineSpacing: 0 })
         content.height = textSize
-        const button = this.add.rectangle(x, y, content.width + padding, content.height + padding, 0xff0000)
+        const UIBox = this.add.rectangle(x, y, Math.ceil((content.width + padding) / this.TILE_SIZE) * this.TILE_SIZE, content.height + padding, 0xff0000)
 
         content.setOrigin(0).setZ(this.BUTTON_LAYER + 100).setDepth(this.BUTTON_LAYER + 1)
-        button.setOrigin(0).setZ(this.BUTTON_LAYER).setDepth(this.BUTTON_LAYER)
+        UIBox.setOrigin(0).setZ(this.BUTTON_LAYER).setDepth(this.BUTTON_LAYER)
+
+        const button = { content, UIBox }
+        UIBox.setInteractive().on('pointerdown', result);
 
         return button
+    }
+
+    moveTime() {
+        console.log('peepee')
     }
 }
 
