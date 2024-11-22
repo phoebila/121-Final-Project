1e69e15a0b4b4508c26f4726f873327c962cf8aa
Jack Sims <123803257+sym-z@users.noreply.github.com>
2024-11-21 17:49:40 -0800

    Went back to using a set to keep references to the actual plant objects.

--- src/prefabs/GameState.js
+++ src/prefabs/GameState.js
@@ -1,39 +1,42 @@
 // When a plant is created, reaped, or had its growth level increased, it needs to be represented in this.totalPlants
-// Uses the Array of Structures format, got help from Brace on strategy and the find/findIndex functions.
 class GameState{
     // If three or more plants are level three growth or above, the game is won.
     static WINNING_PLANT_COUNT = 3
     static WINNING_GROWTH_LEVEL = 3
     constructor(){
-        this.plants = []
+        this.totalPlants = new Map();
     }
     // Used to add new plants to the state of the game, and update an existing plant's growth level.
     addPlantToState(plant){
-        if(this.plants.find(p=>p.position === plant.position)){
-            this.plants.find(p=>p.position === plant.position).growthLevel = plant.growthLevel;    
-        }
-        else{
-            this.plants.push({position:plant.position,growthLevel:plant.growthLevel})
-        }
+        this.totalPlants.set(plant.position, plant);
     }
     // Used when a plant is reaped.
     removePlantFromState(plant){
-        const index = this.plants.findIndex(p => p.position === plant.position);
-        assert(index != -1, "Invalid index used in removePlantFromState()");
-        this.plants.splice(index,1)
+        assert(this.totalPlants.get(plant.position), "Plant not found in Game State")
+
+        if(this.totalPlants.get(plant.position)){
+        this.totalPlants.delete(plant.position)
+        }
+
     }
     // Prints the current state of all plants in the field.
     debugState(){
-        this.plants.forEach((value) => {
+        this.totalPlants.forEach((value) => {
             console.log("[P]: Position: ", value.position, " Growth Level: ", value.growthLevel)
         })
+    }
+    getPlantAtPosition(position)
+    {
+        if(this.totalPlants.get(position)){
+            return this.totalPlants.get(position)       
+        }
     }
     // Returns true or false based on whether the game has been completed or not.
     checkWinCondition()
     {
         let gameWon = true;
-        if (this.plants.size < GameState.WINNING_PLANT_COUNT) gameWon = false;
-        this.plants.forEach((value) => {
+        if (this.totalPlants.size < GameState.WINNING_PLANT_COUNT) gameWon = false;
+        this.totalPlants.forEach((value) => {
             if(value.growthLevel < GameState.WINNING_GROWTH_LEVEL) gameWon = false;
         });
         return gameWon;
